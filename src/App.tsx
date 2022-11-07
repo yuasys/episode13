0c353ed5e0bae6dce0b2bf0190ef3378551466e3
Nob Yuasys <yuasys@gmail.com>
2022-11-07 15:10:54 +0900

    Update Person app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { Person } from "./components/Person";
+import { Person, Country } from "./components/Person";
 
 function App() {
   return (
@@ -11,6 +11,7 @@ function App() {
         age={21}
         isMarried={true}
         friends={["jake", "jassica", "jerry"]}
+        country={Country.France}
       />
     </div>
   );
--- src/components/Person.tsx
+++ src/components/Person.tsx
@@ -7,18 +7,25 @@ interface Props {
   country?: string; //プロパティ名に続いて'?'をつけるとオプション扱いにできる。つけないと必須になる
 }
 
+export enum Country {
+  Brazil = "Brazil",
+  Canada = "Canada",
+  France = "France",
+}
+
 export const Person = (props: Props) => {
   return (
     <div>
       <h1>Name: {props.name}</h1>
       <h1>Email: {props.email}</h1>
       <h1>Age: {props.age}</h1>
-      <h1>This person {props.isMarried ? "is":"is not"}</h1>
+      <h1>This person {props.isMarried ? "is" : "is not"}</h1>
       {props.friends.map((friend: string) => (
         <h1>{friend}</h1>
       ))}
 
+      <h1> Country: {props.country}</h1>
       {/* <h1> Country: {props.country}</h1> */}
     </div>
-  )
-}
+  );
+};
