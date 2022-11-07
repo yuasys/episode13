df0fb3b86199551d9052e06271503604c3e45741
Nob Yuasys <yuasys@gmail.com>
2022-11-08 06:55:58 +0900

    Update npm module

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
-import React from "react";
-import "./App.css";
-import { Person, Country } from "./components/Person";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { Home } from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <Person
-        name="Pedro"
-        email="pedro@gmail.com"
-        age={21}
-        isMarried={true}
-        friends={["jake", "jassica", "jerry"]}
-        country={Country.France}
-      />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
